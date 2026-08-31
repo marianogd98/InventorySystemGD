50cdc572361d34416b9ce24333ad0b70f76a1fc5
Mariano Garcia <magdschiffer1998@gmail.com>
2026-08-31 02:51:20 -0400

    feat: setup CI/CD pipelines with GitHub Actions and Git Flow

--- Inventory.Application/obj/Debug/net8.0/Inventory.Application.AssemblyInfo.cs
+++ Inventory.Application/obj/Debug/net8.0/Inventory.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49177ff6d87a3637ad292eb7cce2f66d99e30d77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd25816c935dcebc0c30c7574b554b0e84e45629")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
